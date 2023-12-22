f2f7472ec6d30e02ad1fb06f20142abbd7de7ad2
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-22 17:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let year=prompt();
+function daysOfAYear(y) {
+	if(((y%4==0)&&(y%100!=0))||y%400==0){
+		return 366;
+	}
+	return 365;
+} 
+alert(daysOfAYear(Number(year)));
